6bd10c985e1b5e77a2cbfd12d7c850954f845bbe
Ayush-Singh-Bhati <ayushbhati737@gmail.com>
2023-11-05 10:00:43 +0530

    carousel into slider

--- src/App.js
+++ src/App.js
@@ -4,43 +4,43 @@ import './App.css';
 import React, { useState } from 'react'
 import Navbar from './componants/Navbar';
 import News from './componants/News';
-import Carousel from './componants/Carousel';
-import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Slider from './componants/Slider'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-const App =()=> {
- const pageSize = 5
+const App = () => {
+  const pageSize = 5
   const apiKey = process.env.REACT_APP_NEWS_API
   const country = 'in'
 
 
   const [progress, setProgress] = useState(0)
 
- 
-  
- 
-    return (
-      <BrowserRouter>
+
+
+
+  return (
+    <BrowserRouter>
       <div>
-      <LoadingBar height= {3} color='red'  progress={progress}/>
-        <Navbar/>
-        
-        <Carousel/>
+        <LoadingBar height={3} color='red' progress={progress} />
+        <Navbar />
+
+    <Slider/>
 
         <Routes>
-          <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey}  pageSize={pageSize} country={country} category='general' />}/>
-          <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country={country} category='business' />}/>
-          <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={country} category='entertainment' />}/>
-          <Route exact path='/general' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country={country} category='general' />}/>
-          <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country={country} category='health' />}/>
-          <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country={country} category='science' />}/>
-          <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country={country} category='sports' />}/>
-          <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country={country} category='technology' />}/>
-                  </Routes>
+          <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey} pageSize={pageSize} country={country} category='general' />} />
+          <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country={country} category='business' />} />
+          <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={country} category='entertainment' />} />
+          <Route exact path='/general' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country={country} category='general' />} />
+          <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country={country} category='health' />} />
+          <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country={country} category='science' />} />
+          <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country={country} category='sports' />} />
+          <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country={country} category='technology' />} />
+        </Routes>
       </div>
-      </BrowserRouter>
-    )
-  
+    </BrowserRouter>
+  )
+
 }
 
 export default App;
---
+++ src/componants/Slider.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import worldmap from './images/worldmap.jpg'
+import newsdogwall from './images/newsdogwall.jpg'
+import newscategory from './images/newscategory.jpg'
+import style from '../modules/carousel.module.css'
+
+export default function Carousel() {
+    return (
+        <div>
+            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" >
+                <div className="carousel-inner" style={{ height: "40rem" }}>
+                    <div className="carousel-item active" style={{ height: "40rem" }}>
+                        <img src={newsdogwall} className={`d-block  ${style.bgImg}`} alt="..." />
+                        <div className={style.shadow}></div>
+                        <div className="container">
+                            <div className="carousel-caption text-start" >
+                                <h1 className={style.text} >TOP HEADLINES.</h1>
+                                <p className={style.Ptext}>We sniff out top headlines for you.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item" style={{ height: "40rem" }}>
+                        <img src={worldmap} className={`d-block  ${style.bgImg}`} alt="..." />
+                        <div className={style.shadow}></div>
+                        <div className="container">
+                            <div className="carousel-caption">
+                                <h1 className={style.text}>WORLDWIDE.</h1>
+                                <p className={style.Ptext}>We are providing you worldwide top latest news.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item" style={{ height: "40rem" }}>
+                        <img src={newscategory} className={`d-block  ${style.bgImg}`} alt="..." />
+                        <div className={style.shadow}></div>
+                        <div className="container">
+                            <div className="carousel-caption text-end">
+                                <h1 className={style.text}>ALL-ROUNDER.</h1>
+                                <p className={style.Ptext}>Here you can read different types news at a one place on a one click.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+    )
+}
